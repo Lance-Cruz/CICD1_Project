af299517000e3c457ea455c73136c4928cbde7fb
Lance Cruz <G00428876@atu.ie>
2025-11-07 19:07:18 +0000

    Added basic service create and read methods

--- src/main/java/ie/atu/project/service/PaymentService.java
+++ src/main/java/ie/atu/project/service/PaymentService.java
@@ -1,4 +1,29 @@
 package ie.atu.project.service;
 
+import ie.atu.project.model.Payment;
+import ie.atu.project.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PaymentService {
+    private final PaymentRepository repo;
+
+    public PaymentService (PaymentRepository repo) {
+        this.repo = repo;
+    }
+
+    public Payment create(Payment p) {
+        return repo.save(p);
+    }
+
+    public List<Payment> findAll() {
+        return repo.findAll();
+    }
+
+    public Payment findByPaymentId(Long id) {
+        return repo.findByPaymentId(id)
+                .orElseThrow(() -> new IllegalArgumentException("Payment ID not found"));
+    }
 }
