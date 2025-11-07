41cfdf7c438b1be4a9e1bf868a52f61fa2b979f5
Lance Cruz <G00428876@atu.ie>
2025-11-07 18:57:27 +0000

    Change interface code to allow it to pass

--- src/main/java/ie/atu/project/repository/PaymentRepository.java
+++ src/main/java/ie/atu/project/repository/PaymentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Optional<Payment> findbyPaymentId(String paymentId);
+    Optional<Payment> findByPaymentId(Long paymentId);
 }
