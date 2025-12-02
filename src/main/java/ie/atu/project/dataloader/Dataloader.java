764317f68d1172a175cc51dc17f688429f1897bb
Lance Cruz <G00428876@atu.ie>
2025-12-02 13:40:37 +0000

    Added more test data in dataloader

--- src/main/java/ie/atu/project/dataloader/Dataloader.java
+++ src/main/java/ie/atu/project/dataloader/Dataloader.java
@@ -15,8 +15,14 @@ public class Dataloader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Payment testData1 = new Payment(1L, "50", "Cash", "Euro");
+        Payment testData1 = new Payment(1L, "5", "Cash", "Euro");
+        Payment testData2 = new Payment(2L, "21", "Debit", "Pound");
+        Payment testData3 = new Payment(3L, "10", "Card", "Dollar");
+        Payment testData4 = new Payment(4L, "18", "Cash", "Euro");
 
         paymentRepository.save(testData1);
+        paymentRepository.save(testData2);
+        paymentRepository.save(testData3);
+        paymentRepository.save(testData4);
     }
 }
