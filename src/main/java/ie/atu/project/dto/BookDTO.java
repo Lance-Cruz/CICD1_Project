339a235efcf6e444c01e3b2237b0506a2dff88e3
Lance Cruz <G00428876@atu.ie>
2025-12-10 12:27:40 +0000

    Created BookDTO

---
+++ src/main/java/ie/atu/project/dto/BookDTO.java
@@ -0,0 +1,61 @@
+package ie.atu.project.dto;
+
+public class BookDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String publisher;
+    private Double price;
+
+    public BookDTO() {
+    }
+
+    public BookDTO(Long id, String title, String author, String publisher, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
