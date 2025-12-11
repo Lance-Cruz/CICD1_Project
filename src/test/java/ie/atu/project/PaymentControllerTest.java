0af43196a64a999ff0090d2b6b8f9eb98832d346
Lance Cruz <G00428876@atu.ie>
2025-12-11 18:49:43 +0000

    Added test for getPaymentById

--- src/test/java/ie/atu/project/PaymentControllerTest.java
+++ src/test/java/ie/atu/project/PaymentControllerTest.java
@@ -1,10 +1,42 @@
 package ie.atu.project;
 
+import ie.atu.project.controller.PaymentController;
+import ie.atu.project.feignclient.BookClient;
+import ie.atu.project.feignclient.NotificationClient;
+import ie.atu.project.model.Payment;
+import ie.atu.project.service.PaymentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+// For building the HTTP requests
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// For checking the results of the requests
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// For converting objects to JSON and vice versa
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.awt.print.Book;
+import java.util.Optional;
+
+@WebMvcTest(PaymentController.class)
 public class  PaymentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private PaymentService paymentService;
+
+    @MockitoBean
+    private NotificationClient notificationClient;
+
+    @MockitoBean
+    private BookClient bookClient;
 
     @BeforeEach
     void setUp() {
@@ -17,8 +49,14 @@ public class  PaymentControllerTest {
     }
 
     @Test
-    void getPaymentById() {
+    void getPaymentById() throws Exception {
+        Payment p = new Payment(1L, 1L, "10","Cash", "Euro");
+        when(paymentService.findByPaymentId(1L)).thenReturn(Optional.of(p));
 
+        mockMvc.perform(get("/api/payment/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.amount").value("10"));
     }
 
     @Test
